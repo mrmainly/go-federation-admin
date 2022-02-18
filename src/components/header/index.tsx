e519096b82e4666407b3400f4ebcaee01ffabba9
ernest <ernest_everstov@mail.ru>
2022-02-18 11:35:14 +0900

    made a phone version of the site

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-import { Box, Container, MenuItem } from '@mui/material'
+import { Box, Container, MenuItem, Drawer, IconButton } from '@mui/material'
 import { styled } from '@mui/system'
 import { Link, useNavigate } from 'react-router-dom'
+import MenuIcon from '@mui/icons-material/Menu';
 
 import { ROUTES } from '../../types'
 
@@ -30,14 +31,82 @@ const LinkRouter = styled(Link)({
 
 const Header = () => {
     const navigate = useNavigate()
-
-    return (
-        <HeaderWrapper>
+    const [state, setState] = useState({
+        mobileView: false,
+        drawerOpen: false,
+    });
+    const { mobileView, drawerOpen } = state;
+    useEffect(() => {
+        const setResponsiveness = () => {
+            return window.innerWidth < 550
+                ? setState((prevState) => ({ ...prevState, mobileView: true }))
+                : setState((prevState) => ({ ...prevState, mobileView: false }));
+        };
+        setResponsiveness();
+        window.addEventListener("resize", () => setResponsiveness());
+    }, []);
+    const Logo = () => {
+        return (
+            <MenuItem sx={{ width: 'max-content' }} onClick={() => navigate(ROUTES.HOME)}>
+                <Img src={'/img/rfg_logo_small.svg'} />
+            </MenuItem>
+        )
+    }
+    const Mobile = () => {
+        const handleDrawerOpen = () =>
+            setState((prevState) => ({ ...prevState, drawerOpen: true }));
+        const handleDrawerClose = () =>
+            setState((prevState) => ({ ...prevState, drawerOpen: false }));
+        return (
+            <MyContainer>
+                <IconButton
+                    {...{
+                        edge: "start",
+                        "aria-label": "menu",
+                        "aria-haspopup": "true",
+                        onClick: handleDrawerOpen,
+                    }}
+                    style={{ color: '#1B1642' }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Logo />
+                <Drawer
+                    {...{
+                        anchor: "left",
+                        open: drawerOpen,
+                        onClose: handleDrawerClose,
+                    }}
+                >
+                    <Box style={{
+                        width: 200, padding: 15, display: 'flex', flexDirection: 'column',
+                        height: '100%'
+                    }}>
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <LinkRouter to={ROUTES.USERS}>
+                                <MenuItem>
+                                    Пользователи
+                                </MenuItem>
+                            </LinkRouter>
+                            <LinkRouter to={ROUTES.TOURNAMENTS}>
+                                <MenuItem>
+                                    Турниры
+                                </MenuItem>
+                            </LinkRouter>
+                            <MenuItem>
+                                <LinkRouter to="/sd">На сайт</LinkRouter>
+                            </MenuItem>
+                        </Box>
+                    </Box>
+                </Drawer>
+            </MyContainer>
+        )
+    }
+    const Desktop = () => {
+        return (
             <MyContainer>
                 <Box sx={{ display: 'flex' }}>
-                    <MenuItem sx={{ width: 'max-content' }} onClick={() => navigate(ROUTES.HOME)}>
-                        <Img src={'/img/rfg_logo_small.svg'} />
-                    </MenuItem>
+                    <Logo />
                     <LinkRouter to={ROUTES.USERS}>
                         <MenuItem>
                             Пользователи
@@ -55,6 +124,11 @@ const Header = () => {
                     </MenuItem>
                 </Box>
             </MyContainer>
+        )
+    }
+    return (
+        <HeaderWrapper>
+            {mobileView ? Mobile() : Desktop()}
         </HeaderWrapper>
     )
 }
